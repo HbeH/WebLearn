8da169ed7c00d9cbe6063d0340b15f286f5c909d
Hbe <467053290@qq.com>
2016-04-02 13:29:30 +0800

    Revert "Revert "妙味""
    This reverts commit f9750a4f596d5b2da1aae989d3fb3880002e92fc.

---
+++ 妙味课堂/js初级教程/js热身练习/js/my_js.js
@@ -0,0 +1,10 @@
+var set_But = document.getElementById('set_but');
+var on_Show = document.getElementById('On_show');
+
+set_But.onmouseover = show;
+
+function show(){
+	on_Show.style.display = 'block';
+	
+}
+
